9e7542d6c875bd6c2148952c0ad861b1c7f62b4c
Shirdora <112417754+Awa-RunA@users.noreply.github.com>
2022-11-17 12:54:46 -0500

    Update main.cpp
    I finished adding all the case statements to the switch statement. I asked do we really need the have in the menu to sort by title or author when you can view by author and title. I fell like that is the same as sorting them.

--- main.cpp
+++ main.cpp
@@ -14,13 +14,18 @@ void Menudisplay(){
   cout << "1. Add a book\n";
   cout << "2. View books by Title\n";
   cout << "3. View books by Author Name\n";
-  cout << "4. Edit a book\n";
-  cout << "5. Sort by Title\n";
-  cout << "6. Sort by Author\n";
+  cout << "4. Edit a book\n"; 
+  cout << "5. Sort by Title\n";  //don't think these are needed
+  cout << "6. Sort by Author\n";  //don't think these are needed
   cout << "7. Delete a book\n";
   cout << "8. Find a book\n";
   cout << "9. Exit Application\n";
   
+}
+void updateMenu(){
+ cout<<"\n1. Update Title \n";
+ cout<<"2. Update Author \n";
+  cout<<"3. Update  Title and Author \n";
 }
 //Faith Olajide
 void addBook(){};
@@ -30,7 +35,8 @@ void update(){};
 
 
 int main(){
-    int opp;
+ int opp;
+    int choi;
     string title;
     string author;
     string ISBN;
@@ -39,23 +45,70 @@ int main(){
         Menudisplay();
         cin>>opp;
 
-        switch(case){
+        switch(opp){
             deafult:
               cout<<"Please Enter a Valid Option!";
              break;
-            case 1:
+            case 1: //Add Book
               cout<<"Enter the Book's Title: ";
-              cin>>title;
+              cin.ignore();
+              getline(cin,title);
               cout<<"Enter the Book's Author: ";
-              cin>>author
+              cin.ignore();
+              getline(cin,author);
               cout<<"Enter the Book's ISBN: ";
               cin>>ISBN;
               break;
+          case 2: //View by Title
+             
+            break;
+          case 3: //view by author Name
+            
+            break;
+          case 4: //edit book
+            cout<<"Enter the ISBN for the Book you would like to edit: ";
+              cin>>ISBN;
+            cout<<"What would you to change?: ";
+             updateMenu();
+              cin>>choi;
+              switch(choi){
+                default:
+                  cout<<"Enter a Valid option!";
+                  break;
+                case 1:
+                  cout<<"Enter the New Book Title: ";
+                  cin.ignore();
+                   getline(cin,title);
+                  break;
+                case 2:
+                  cout<<"Enter the New Author: ";
+                  cin.ignore();
+                  getline(cin,author);
+                  break;
+                case 3:
+                  cout<<"Enter the New Book Title: ";
+                  cin.ignore();
+                  getline(cin,title);
+                  cout<<"Enter the New Author: ";
+                  cin.ignore();
+                  getline(cin,author);
+                  break;
+            }
+            break;
+          case 7://delete the book
+            cout<<"Enter the ISBN for the book to be deleted: ";
+              cin>>ISBN;
+              
+            break;
+          case 8:
+            cout<<"Enter the ISBN of the book to find: ";
+            cin>>ISBN;
+          break;
+          case 9:
+          cout<<"Enjoy The Library!"<<endl;
         }
        
       }
 
 }
-
-
 
